c8ffd2092764f04351487c49ac195f0521d7dd55
EderHdz-Studio <125609785+EdyDoto@users.noreply.github.com>
2026-02-17 21:55:51 -0600

    chore(config): update project configuration for server-side integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ export default defineConfig({
     resolve: {
       alias: {
         '@components': '/src/components',
+        '@server': '/src/server',
         '@layouts': '/src/layouts',
         '@lib': '/src/lib',
         '@pages': '/src/pages',
